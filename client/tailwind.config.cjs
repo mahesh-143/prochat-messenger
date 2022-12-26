0d8742ba22bcc7f83fb5ff475d71bd94380cf473
mahesh-143 <maheshodedara13@yahoo.com>
2022-12-26 20:56:16 +0530

    configured tailwind css

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Inter : "Inter, sans-serif",
+    },
+    extend: {
+      colors: {
+        accent : "var(--clr-accent)",
+        input : "var(--clr-input)",
+        body : "var(--clr-body)",
+        dark : "var(--clr-dark-100)",
+        darker : "var(--clr-dark-200)",
+        error : "var(--clr-error)",
+      }
+    },
+  },
+  plugins: [],
+}
