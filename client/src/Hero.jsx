ba2aea3d6c6ae0a12280db2d7ee072a5bb90ab2f
mahesh-143 <maheshodedara13@yahoo.com>
2022-12-30 21:22:23 +0530

    added hero text

---
+++ client/src/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="w-[50%] p-16 bg-darker h-screen hidden lg:flex flex-col items-center justify-center">
+    <h1 className="text-6xl max-w-md pb-6">ProChat -  A Platform for Online chating</h1>
+    <p className="text-xl max-w-md">Find friends and create chatrooms to enjoy real time messeging</p>
+    </div>
+  )
+}
+
+export default Hero
