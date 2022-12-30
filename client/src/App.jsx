4beea21313052acdfbcda36b08551faadecbc52a
mahesh-143 <maheshodedara13@yahoo.com>
2022-12-30 21:23:48 +0530

    designed login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import Hero from "./components/forms/Hero"
 import Login from "./components/forms/Login"
+import LoginForm from "./components/forms/LoginForm"
 
 function App() {
-  return <Login />
+  return (
+    <div className="flex items-center justify-center m-auto">
+    <Hero />
+    <Login />
+    </div>
+  )
 }
 
 export default App
