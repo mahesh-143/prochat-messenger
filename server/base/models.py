3cf2c682bbd3f92f8d157c2988ea9c804fac3c3a
mahesh-143 <maheshodedara13@yahoo.com>
2023-01-02 19:56:13 +0530

    created user model

--- server/base/models.py
+++ server/base/models.py
@@ -1,19 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 import uuid
 
-# Create your models here.
-
-
-class User(models.Model):
+class User(AbstractUser):
     class Meta:
-        db_table = "User"
+        db_table = 'User'
 
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False)
-    username = models.TextField(unique=True)
-    password = models.TextField()
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True, null=False, blank=False, default="", db_index=True)
+    password = models.CharField(max_length=255)
     bio = models.TextField(null=True)
-    verified = models.BooleanField()
-    is_active = models.BooleanField()
+    verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
--- server/server/settings.py
+++ server/server/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig',
+    'base',
 
     'rest_framework',
 ]
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'base.User'
